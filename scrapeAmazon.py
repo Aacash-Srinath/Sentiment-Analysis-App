d338b0124c0c7309cd3cbfd4307127b1cb36614b
Aacash Srinath Nambiar <100955640+Aacash-Srinath@users.noreply.github.com>
2023-06-25 11:15:49 +0530

    Update scrapeAmazon.py

--- scrapeAmazon.py
+++ scrapeAmazon.py
@@ -5,16 +5,16 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.service import Service
 import sys
 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
 def getReviews(asin):
     # Set up the web driver
-    options = Options()
+    options = webdriver.ChromeOptions()
     options.add_argument('--disable-gpu')
     options.add_argument('--headless')
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
