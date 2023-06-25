397c60c6c1bb2e906dc09988d40f9582bc265492
Aacash Srinath Nambiar <100955640+Aacash-Srinath@users.noreply.github.com>
2023-06-25 10:51:37 +0530

    Update scrapeAmazon.py

--- scrapeAmazon.py
+++ scrapeAmazon.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import streamlit as st
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
+from webdrivermanager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
