2ed1b2234014aea0331f1d03abd14347c30facce
Aacash Srinath Nambiar <100955640+Aacash-Srinath@users.noreply.github.com>
2023-06-25 11:27:05 +0530

    Update scrapeAmazon.py

--- scrapeAmazon.py
+++ scrapeAmazon.py
@@ -16,7 +16,7 @@ def getReviews(asin):
     # Set up the web driver
     options = webdriver.ChromeOptions()
     options.add_argument('--headless')
-    driver = webdriver.Chrome('chromedriver.exe', options=options)
+    driver = webdriver.Chrome('chromedriver.exe')
 
     # Navigate to the product page
     product_url = f"https://www.amazon.in/dp/{asin}"
